f4f05e3fccb2647bb651d30d001d282aa9209798
pantzyy <pzani1989@gmail.com>
2022-04-26 18:11:07 +0800

    The project initialization

---
+++ scheduler/src/main/java/com/crypto/currency/scheduler/controller/SystemController.java
@@ -0,0 +1,58 @@
+package com.crypto.currency.scheduler.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author Panzi
+ * @Description Health check and shut down the system
+ * @date 2022/4/26 16:49
+ */
+@RestController @RequestMapping("/system") public class SystemController {
+
+    private final ApplicationContext context;
+
+    private static final String OK = "I am OK!";
+    private static final String NOT_OK = "Not health!";
+    private static final String SHUT_DOWN = "Shutting Down in 15 seconds!";
+
+    private static final AtomicBoolean HEALTH = new AtomicBoolean(true);
+
+    public SystemController(ApplicationContext context) {
+        this.context = context;
+    }
+
+    @GetMapping("/shutdown") public ResponseEntity<String> shutdownApp() {
+        HEALTH.set(false);
+
+        new Thread(() -> {
+            System.out.println("Shutdown Hook is running !");
+            try {
+                Thread.sleep(1000 * 15);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.exit(SpringApplication.exit(context, () -> 0));
+        }).start();
+
+        System.out.println("Application Terminating ...");
+        return new ResponseEntity<>(SHUT_DOWN, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/health", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.HEAD})
+    public ResponseEntity<String> health() {
+        if (HEALTH.get()) {
+            return new ResponseEntity<>(OK, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(NOT_OK, HttpStatus.BAD_GATEWAY);
+        }
+    }
+}
---
+++ scheduler/src/test/java/com/crypto/currency/scheduler/SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto.currency.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
